1e39a5d333eae0f526c9415068251a62fc8b29ac
Cafezinhu <aertoncastro@gmail.com>
2020-07-30 13:40:06 -0300

    dinosaur search

--- controllers/dinosaur-controller.ts
+++ controllers/dinosaur-controller.ts
@@ -1,15 +1,34 @@
 import { Response, Request } from 'https://deno.land/x/oak/mod.ts';
-import { getDinosaurs } from '../models/dinosaur.ts';
+import { getDinosaurs, IDinosaur } from '../models/dinosaur.ts';
 
 const dinosaurs = getDinosaurs();
 
 export class DinosaurController{
+
+    private static _search(name: string): IDinosaur | undefined{
+        return dinosaurs.filter(dinosaur => dinosaur.name.toLowerCase().includes(name.toLowerCase()))[0];
+    }
+
     static getAll({response}: {response: Response}): void{
         ok(response, dinosaurs);
+    }
+
+    static get({params, response}: {params:{name: string}, response: Response}): void{
+        let dinosaur: IDinosaur | undefined = this._search(params.name);
+        if(dinosaur){
+            ok(response, dinosaur);
+        }else{
+            notFound(response, "Dinosaur not found");
+        }
     }
 }
 
 function ok(response: Response, body: any){
     response.status = 200;
     response.body = body;
+}
+
+function notFound(response: Response, message: string){
+    response.status = 404;
+    response.body = {message: message};
 }
--- routes/routes.ts
+++ routes/routes.ts
@@ -4,6 +4,7 @@ import { DinosaurController } from '../controllers/dinosaur-controller.ts';
 
 const router = new Router();
 
-router.get("/api/v1/dinosaurs", DinosaurController.getAll);
+router.get("/api/v1/dinosaurs", DinosaurController.getAll)
+    .get("/api/v1/dinosaurs/:name", DinosaurController.get.bind(DinosaurController));
 
 export default router;
